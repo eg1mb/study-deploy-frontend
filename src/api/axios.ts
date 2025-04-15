4a4c39a944b7ee4907cdfa007b81003f6af2b48c
eg1mb <eg1mb@naver.com>
2025-04-15 23:40:57 +0900

    fix: axios.ts 수정

--- src/api/axios.ts
+++ src/api/axios.ts
@@ -3,6 +3,7 @@ import type { AxiosError } from 'axios';
 
 const instance = axios.create({
   baseURL: 'https://port-0-study-deploy-backend-m9ihub5nb21ae68f.sel4.cloudtype.app',
+  withCredentials: true,
   headers: {
     'Content-Type': 'application/json',
   },
