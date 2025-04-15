f7741dd06301c0733f802ab31b6dc2119b22a4e5
Gyumin_Kim <145199011+eg1mb@users.noreply.github.com>
2025-04-15 23:10:22 +0900

    Update axios.ts

--- src/api/axios.ts
+++ src/api/axios.ts
@@ -2,7 +2,7 @@ import axios from 'axios';
 import type { AxiosError } from 'axios';
 
 const instance = axios.create({
-  baseURL: 'https://port-0-study-deploy-backend-m9ihub5nb21ae68f.sel4.cloudtype.app',
+  baseURL: 'https://port-0-study-deploy-backend-m9iks1vld34c7422.sel4.cloudtype.app',
   headers: {
     'Content-Type': 'application/json',
   },
